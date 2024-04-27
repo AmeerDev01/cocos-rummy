38698dfb982c310f7c9153cb993229f22d5cc54f
UranusQeen <549084389@qq.com>
2024-04-27 21:51:54 +0800

    删除 img_rule_in 所属的 auto_gxfc 图集，修改小火箭进入bug

--- assets/script/subGame/crashV2/components/crashV2_game_teen.ts
+++ assets/script/subGame/crashV2/components/crashV2_game_teen.ts
@@ -22,13 +22,13 @@ export class CrashV2_game_teen extends BaseComponent<IState, IProps, IEvent> {
 	}
 
 	protected propertyNode = {
-		props_Teen_List: new Node(),
-		props_recordItem: new Node(),
-		props_one: new Node(),
-		props_tow: new Node(),
-		props_there: new Node(),
-		props_four: new Node(),
-		props_five: new Node(),
+		// props_Teen_List: new Node(),
+		// props_recordItem: new Node(),
+		// props_one: new Node(),
+		// props_tow: new Node(),
+		// props_there: new Node(),
+		// props_four: new Node(),
+		// props_five: new Node(),
 	}
 
 	public props: IProps = {
@@ -46,21 +46,21 @@ export class CrashV2_game_teen extends BaseComponent<IState, IProps, IEvent> {
 	}
 	//初始化节点
 	initNode(){
-		for(let i=0;i<99;i++){
-			let node= instantiate(this.propertyNode.props_recordItem)
-			if(i==98){
-				node.getChildByName('record_new').active=true
-			}
-			this.propertyNode.props_Teen_List.addChild(node)
-		}
+		// for(let i=0;i<99;i++){
+		// 	let node= instantiate(this.propertyNode.props_recordItem)
+		// 	if(i==98){
+		// 		node.getChildByName('record_new').active=true
+		// 	}
+		// 	this.propertyNode.props_Teen_List.addChild(node)
+		// }
 	}
 	/** 渲染底部数字 */
 	renderTheBottomNumber(){
-		this.propertyNode.props_one.getComponent(Label).string='1'
-		this.propertyNode.props_tow.getComponent(Label).string='1'
-		this.propertyNode.props_there.getComponent(Label).string='1'
-		this.propertyNode.props_four.getComponent(Label).string='1'
-		this.propertyNode.props_five.getComponent(Label).string='1'
+		// this.propertyNode.props_one.getComponent(Label).string='1'
+		// this.propertyNode.props_tow.getComponent(Label).string='1'
+		// this.propertyNode.props_there.getComponent(Label).string='1'
+		// this.propertyNode.props_four.getComponent(Label).string='1'
+		// this.propertyNode.props_five.getComponent(Label).string='1'
 	}
 	protected bindEvent(): void {
 		this.node.getChildByName('props_close').on(Node.EventType.TOUCH_END, () => {
