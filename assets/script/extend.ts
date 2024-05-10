fd89436fd5205a8f3e36f89b1ad38bc7d48a4d38
Simple <SimpleMorden@outlook.com>
2024-05-10 14:38:42 +0800

    删除可能引起卡死的spine动画，增加js四则运算拓展

--- assets/script/extend.ts
+++ assets/script/extend.ts
@@ -88,6 +88,45 @@ Node.prototype.getDeepChildByName = function (nodeName: string): Node | undefine
 	}
 }
 
+//除法函数，用来得到精确的除法结果
+Number.prototype.div = function (arg1, arg2): number {
+	var t1 = 0, t2 = 0, r1, r2;
+	try { t1 = arg1.toString().split(".")[1].length } catch (e) { }
+	try { t2 = arg2.toString().split(".")[1].length } catch (e) { }
+	r1 = Number(arg1.toString().replace(".", ""))
+	r2 = Number(arg2.toString().replace(".", ""))
+	return (r1 / r2) * Math.pow(10, t2 - t1);
+}
+
+//乘法函数，用来得到精确的乘法结果
+Number.prototype.mul = function (arg1, arg2): number {
+	var m = 0, s1 = arg1.toString(), s2 = arg2.toString();
+	try { m += s1.split(".")[1].length } catch (e) { }
+	try { m += s2.split(".")[1].length } catch (e) { }
+	return Number(s1.replace(".", "")) * Number(s2.replace(".", "")) / Math.pow(10, m)
+}
+
+//加法函数，用来得到精确的加法结果
+Number.prototype.add = function (arg1, arg2): number {
+	var r1, r2, m;
+	try { r1 = arg1.toString().split(".")[1].length } catch (e) { r1 = 0 }
+	try { r2 = arg2.toString().split(".")[1].length } catch (e) { r2 = 0 }
+	m = Math.pow(10, Math.max(r1, r2))
+	return (arg1 * m + arg2 * m) / m
+}
+
+//减法函数，用来得到精确的减法结果
+Number.prototype.sub = function (arg1, arg2): number {
+	var r1, r2, m, n;
+	try { r1 = arg1.toString().split(".")[1].length } catch (e) { r1 = 0 }
+	try { r2 = arg2.toString().split(".")[1].length } catch (e) { r2 = 0 }
+	m = Math.pow(10, Math.max(r1, r2));
+	//last modify by deeka
+	//动态控制精度长度
+	n = (r1 >= r2) ? r1 : r2;
+	return Number(((arg1 * m - arg2 * m) / m).toFixed(n));
+}
+
 window.addEventListener(
 	"unhandledRejectedPromise",
 	function browserRejectionHandler(event) {
