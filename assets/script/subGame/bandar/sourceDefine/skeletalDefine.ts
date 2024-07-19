080075ae4ae5173aff2663abbba51bfb5408c1af
Ameer Rizvi <63542458+AmeerRizvi@users.noreply.github.com>
2024-07-19 11:54:19 +0400

    games removed, only gow

--- assets/script/subGame/bandar/sourceDefine/skeletalDefine.ts
+++
@@ -1,9 +0,0 @@
-import { Prefab, sp } from "cc"
-import { loopFiles } from "../../../base/SourceManage"
-
-//动画资源 路径
-export enum SkeletalPathDefine {
-
-}
-
-export default (bundlePkgName: string) => loopFiles(bundlePkgName, sp.SkeletonData, SkeletalPathDefine)
--- assets/script/subGame/rummy/sourceDefine/skeletalDefine.ts
+++
@@ -1,8 +0,0 @@
-import { Prefab, sp } from "cc"
-import { loopFiles } from "../../../base/SourceManage"
-
-export enum SkeletalPathDefine {
-  
-}
-
-export default (bundlePkgName: string) => loopFiles(bundlePkgName, sp.SkeletonData, SkeletalPathDefine)
